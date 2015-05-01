dcac4ee600938a5cc955f7dbfffef104826eadfd
Tom Deakin <thomasdeakin@gmail.com>
2015-05-01 19:23:51 +0100

    Include cmath in bilateral.cpp on non-windows

--- solutions/Bilateral/bilateral.cpp
+++ solutions/Bilateral/bilateral.cpp
@@ -8,6 +8,10 @@
 #include <sstream>
 #include <vector>
 
+#if !defined(WIN32) && !defined(_WIN32) && !defined(__WIN32) || defined(__CYGWIN__)
+  #include <cmath>
+#endif
+
 #include <SDL2/SDL.h>
 
 #define __CL_ENABLE_EXCEPTIONS
