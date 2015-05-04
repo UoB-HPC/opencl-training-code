e437da7e003d295dcfcca09c891463d177b69d57
Tom Deakin <tom.deakin@bristol.ac.uk>
2015-05-04 16:26:46 +0100

    Include and link against GLEW in Linux for NBody-GL-VBO

--- solutions/NBody-GL-VBO/nbody.cpp
+++ solutions/NBody-GL-VBO/nbody.cpp
@@ -7,7 +7,7 @@
 #include <sstream>
 #include <vector>
 
-#if defined(_WIN32)
+#if defined(_WIN32) || !defined(__APPLE__)
   #define GLEW_STATIC
   #include <GL/glew.h>
 #endif
@@ -15,6 +15,12 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 #undef main
+
+#if !defined(_WIN32) && !defined(__APPLE__)
+    #include <GL/gl.h>
+    #include <GL/glx.h>
+#endif
+
 
 #define __CL_ENABLE_EXCEPTIONS
 #include <cl.hpp>
