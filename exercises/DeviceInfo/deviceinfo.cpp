a0911250c527f0ffd780341cd0d1e961da0472d4
Tom Deakin <thomasdeakin@gmail.com>
2015-05-19 10:02:26 +0100

    Add license to cpp source file

--- exercises/DeviceInfo/deviceinfo.cpp
+++ exercises/DeviceInfo/deviceinfo.cpp
@@ -8,6 +8,16 @@
  *          Updated by Tom Deakin, August 2013
 */
 
+/*
+ *
+ * This code is released under the "attribution CC BY" creative commons license.
+ * In other words, you can use it in any way you see fit, including commercially,
+ * but please retain an attribution for the original authors:
+ * the High Performance Computing Group at the University of Bristol.
+ * Contributors include Simon McIntosh-Smith, James Price, Tom Deakin and Mike O'Connor.
+ *
+ */
+
 #include <iostream>
 #include <vector>
 
