85f5ec33ef33490de86f89333842dae6fcf11966
James Price <j.price@bristol.ac.uk>
2018-11-26 15:09:02 +0000

    Tweak NBody-GL header order to fix MSVC build

--- exercises/NBody-GL/nbody.cpp
+++ exercises/NBody-GL/nbody.cpp
@@ -17,14 +17,14 @@
 #include <sstream>
 #include <vector>
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
-#undef main
-
 #define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_TARGET_OPENCL_VERSION 120
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #include <CL/cl2.hpp>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#undef main
 
 
 #if !defined(_WIN32) && !defined(__APPLE__)
